5bbdb504b576b5271c37f89c1f4dfaef3044e81c
Ronaldozb <fzelayab@unah.hn>
2019-07-19 09:27:03 -0600

    creando clases ciudad y clientes

---
+++ tarea 2/tarea 2/clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_2
+{
+    public class clientes
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public ciudades Ciudad { get; set; }
+        public string Direccion { get; set; }
+    }
+}
